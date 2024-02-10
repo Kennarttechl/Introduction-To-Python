616fb3a4b25299fe364178d5af97b107a27f450e
Kennarttechl <kennarttechnologies@gmail.com>
2024-02-09 22:25:54 -0800

    Tutorial

---
+++ app.py
@@ -0,0 +1,43 @@
+My_name = ["John", "Benjamin", "Tim"]
+
+# output = ["banana", "mango"]
+
+# print(output)
+# print(My_name)
+
+
+
+# Example                       Data type
+# ============                  ==============
+
+# Text type 
+# x = "John"                      str
+
+
+# # Numeric Types
+# x = 22                          int
+# x = 33.0                        float
+# x = 1j                          complex
+
+# # Sequence Types
+# x = ["Apple", "Mango", "Guava"]     list
+# x = ("Apple", "Mango", "Guava")     tuple
+# x = range(10)                       range
+
+# # Mapping Type 
+# x = {"Age": 22, "Country": "USA"}   dict
+
+# # Set Types
+# x = {"Age", "country", "usa"}       set
+# x = ({"Age", "Country"})            frozenset
+
+
+# # Boolean Type
+# x = True                            bool
+# x = False
+
+my_grocery_store = ["Mango", "Apple", "Bananna"]
+
+my_grocery_store.append("tech")
+
+print(type(my_grocery_store))
